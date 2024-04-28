192a91c994231fc4142fffe74db8dad354c9fa99
kirandash <thekirankumardash@gmail.com>
2024-04-28 18:18:25 +0800

    9 DB connection in NestJS with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TagModule,
+    // .forRoot() method is used to configure the database connection
+    // TODO: Move the database configuration to a separate file called ormconfig.ts in src. Currently it's not moved because I am unsure about the type
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'mediumclone',
+      password: 'mediumclone',
+      database: 'mediumclone',
+      entities: [],
+      // 🚨 Don't use this in PROD!!!
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
